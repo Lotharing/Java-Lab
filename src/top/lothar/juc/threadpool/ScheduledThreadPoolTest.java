02fbb4f02e778cc2743c32c1c33863ce3a082c36
Lotharing <809676295@qq.com>
2020-08-19 20:11:03 +0800

    JUC/演示Seheduled线程池 / 周期性执行线任务 / core 自定义 max = integer.max.value / DelayWorkQueue队列

--- src/top/lothar/juc/threadpool/ScheduledThreadPoolTest.java
+++ src/top/lothar/juc/threadpool/ScheduledThreadPoolTest.java
@@ -11,7 +11,9 @@ public class ScheduledThreadPoolTest {
 
     public static void main(String[] args) {
         ScheduledExecutorService threadPool = Executors.newScheduledThreadPool(10);
-//        threadPool.schedule(new Task(), 5, TimeUnit.SECONDS);
+        // 5 秒延迟
+        threadPool.schedule(new Task(), 5, TimeUnit.SECONDS);
+        // 1秒执行 之后-> 3秒/次
         threadPool.scheduleAtFixedRate(new Task(), 1, 3, TimeUnit.SECONDS);
     }
 }
