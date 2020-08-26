86d54b2fa579f73944294459d046627162123186
lutong <809676295@qq.com>
2020-08-26 20:04:06 +0800

    ThreadLocal-ThreadLocal保存用户信息在service不用方法中传递使用/因为多个请求的时候不同的用户不同线程拿的用户信息在ThreadLocal中对应线程共享用户信息/不同线程之间的用户信息隔离

---
+++ src/top/lothar/juc/threadlocal/ThreadLocalNormalUsage06.java
@@ -0,0 +1,66 @@
+package top.lothar.juc.threadlocal;
+
+/**
+ * 描述：     演示ThreadLocal用法2：避免传递参数的麻烦
+ */
+public class ThreadLocalNormalUsage06 {
+
+    public static void main(String[] args) {
+        new Service1().process("LT");
+
+    }
+}
+
+/**
+ * 比做过滤器场景 / 从token中拿到用户信息
+ */
+class Service1 {
+    //读取到用户信息 设置到ThreadLocal中国呢
+    public void process(String name) {
+        User user = new User(name);
+        UserContextHolder.holder.set(user);
+        System.out.println("拦截器从Token中获取的User保存在ThreadLocal中："+UserContextHolder.holder.get().name);
+        new Service2().process();
+    }
+}
+
+class Service2 {
+
+    public void process() {
+        //可以直接在ThreadLocal中get出User
+        User user = UserContextHolder.holder.get();
+        ThreadSafeFormatter.dateFormatThreadLocal.get();
+        System.out.println("Service2拿到用户名：" + user.name);
+        new Service3().process();
+    }
+}
+
+class Service3 {
+
+    public void process() {
+        //可以直接在ThreadLocal中get出User
+        User user = UserContextHolder.holder.get();
+        System.out.println("Service3拿到用户名：" + user.name);
+        UserContextHolder.holder.remove();
+    }
+}
+
+/**
+ * ThreadLocal对象 需要是static
+ */
+class UserContextHolder {
+    /**
+     * 没没有创建初始值 在合适的时候（拿到user对象信息时候在set进去）
+     */
+    public static ThreadLocal<User> holder = new ThreadLocal<>();
+
+}
+
+class User {
+
+    String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+}
