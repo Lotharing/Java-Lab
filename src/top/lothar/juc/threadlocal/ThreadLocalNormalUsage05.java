9d41a13b55d88fb8239774049fac06a6a8b0c6dc
lutong <809676295@qq.com>
2020-08-26 14:34:06 +0800

    ThreadLocal-ThreadLocal源码/get set remove initialValue 说明解读

--- src/top/lothar/juc/threadlocal/ThreadLocalNormalUsage05.java
+++ src/top/lothar/juc/threadlocal/ThreadLocalNormalUsage05.java
@@ -6,6 +6,18 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 /**
+ * ThreadLocal定义了四个方法:
+ *
+ * get():返回此线程局部变量当前副本中的值
+ * set(T value):将线程局部变量当前副本中的值设置为指定值
+ * initialValue():返回此线程局部变量当前副本中的初始值
+ * remove():移除此线程局部变量当前副本中的值
+ * ThreadLocal还有一个特别重要的静态内部类ThreadLocalMap，该类才是实现线程隔离机制的关键。get()、set()、remove()都是基于该内部类进行操作，ThreadLocalMap用键值对方式存储每个线程变量的副本，key为当前的ThreadLocal对象，value为对应线程的变量副本。
+ * 试想，每个线程都有自己的ThreadLocal对象，也就是都有自己的ThreadLocalMap，对自己的ThreadLocalMap操作，当然是互不影响的了，
+ * 这就不存在线程安全问题了，所以ThreadLocal是以空间来交换安全性的解决思路。加锁是时间交换安全性
+ *
+ * 每个ThreadLocal内部都有一个ThreadLocalMap,他保存的key是ThreadLocal的实例，他的值是当前线程的局部变量的副本的值。
+ *
  * 目的：     效率提高（池）+节省资源（共享对象+池） + 线程安全 （ThreadLocal复印实例副本，10个线程就有10个属于自己的dateFormat对象）
  * 描述：     利用ThreadLocal，给每个线程分配自己的dateFormat对象，保证了线程安全，高效利用内存
  */
