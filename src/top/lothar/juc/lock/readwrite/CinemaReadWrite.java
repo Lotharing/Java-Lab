94ee6c42b4f8b721f999b207038559ca83899796
lutong <809676295@qq.com>
2020-08-30 21:47:12 +0800

    Lock-ReentrantReadWriteLock读(共享)写(独占/排他)锁 执行流程分析

--- src/top/lothar/juc/lock/readwrite/CinemaReadWrite.java
+++ src/top/lothar/juc/lock/readwrite/CinemaReadWrite.java
@@ -3,7 +3,19 @@ package top.lothar.juc.lock.readwrite;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
- * 描述：     TODO
+ * 描述：     执行流程：
+ *  第一步：
+ *      Thread1得到了读锁，正在读取
+ *      Thread2得到了读锁，正在读取
+ *  第二步：
+ *      Thread1释放读锁
+ *      Thread2释放读锁
+ *      Thread3得到了写锁，正在写入（释放读锁的同时 写得到写锁）
+ *  第三步：
+ *      Thread3释放写锁
+ *      Thread4得到了写锁，正在写入 （写锁释放，其他线程获得到写锁）
+ *  第四步：
+ *      Thread4释放写锁
  */
 public class CinemaReadWrite {
     //读写锁
