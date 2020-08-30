051d9f53e3215ffbb87718f97c180b29f9a1233f
lutong <809676295@qq.com>
2020-08-30 23:02:19 +0800

    Lock-ReentrantReadWriteLock演示公平非公下  - 读不能插队问题(队列头节点是写)

--- src/top/lothar/juc/lock/readwrite/CinemaReadWrite.java
+++ src/top/lothar/juc/lock/readwrite/CinemaReadWrite.java
@@ -52,9 +52,33 @@ public class CinemaReadWrite {
     }
 
     public static void main(String[] args) {
-        new Thread(()->read(),"Thread1").start();
+        /**
+         * 非公平下 插队问题-读不插队情况 头节点问题
+         *
+         * 分析 ： 1 写 释放  -> 2 3 读 释放 （期间5想插队但是 队列头节点4是写 为避免饥饿 不能插队 ）->   4 写 释放 - > 5 读释放
+         *
+         * 执行结果：
+         * Thread1得到了写锁，正在写入
+         * Thread1释放写锁
+         * Thread2得到了读锁，正在读取
+         * Thread3得到了读锁，正在读取
+         * Thread2释放读锁
+         * Thread3释放读锁
+         * Thread4得到了写锁，正在写入
+         * Thread4释放写锁
+         * Thread5得到了读锁，正在读取
+         * Thread5释放读锁
+         */
+        new Thread(()->write(),"Thread1").start();
         new Thread(()->read(),"Thread2").start();
-        new Thread(()->write(),"Thread3").start();
+        new Thread(()->read(),"Thread3").start();
         new Thread(()->write(),"Thread4").start();
+        new Thread(()->read(),"Thread5").start();
+
+        //电影院 共享读 单独写 问题
+//        new Thread(()->read(),"Thread1").start();
+//        new Thread(()->read(),"Thread2").start();
+//        new Thread(()->write(),"Thread3").start();
+//        new Thread(()->write(),"Thread4").start();
     }
 }
