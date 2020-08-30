ddfcf6c244d38df15589069f28d786ec2fa716f9
lutong <809676295@qq.com>
2020-08-30 21:43:52 +0800

    Lock-ReentrantReadWriteLock读(共享)写(独占/排他)锁 读锁线程可以一起去执行读（期间写阻塞）读锁释放 写锁单个获得写锁一一执行任务

---
+++ src/top/lothar/juc/lock/readwrite/CinemaReadWrite.java
@@ -0,0 +1,48 @@
+package top.lothar.juc.lock.readwrite;
+
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * 描述：     TODO
+ */
+public class CinemaReadWrite {
+    //读写锁
+    private static ReentrantReadWriteLock reentrantReadWriteLock = new ReentrantReadWriteLock();
+    //读锁
+    private static ReentrantReadWriteLock.ReadLock readLock = reentrantReadWriteLock.readLock();
+    //写锁
+    private static ReentrantReadWriteLock.WriteLock writeLock = reentrantReadWriteLock.writeLock();
+
+    private static void read() {
+        readLock.lock();
+        try {
+            System.out.println(Thread.currentThread().getName() + "得到了读锁，正在读取");
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println(Thread.currentThread().getName() + "释放读锁");
+            readLock.unlock();
+        }
+    }
+
+    private static void write() {
+        writeLock.lock();
+        try {
+            System.out.println(Thread.currentThread().getName() + "得到了写锁，正在写入");
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println(Thread.currentThread().getName() + "释放写锁");
+            writeLock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Thread(()->read(),"Thread1").start();
+        new Thread(()->read(),"Thread2").start();
+        new Thread(()->write(),"Thread3").start();
+        new Thread(()->write(),"Thread4").start();
+    }
+}
