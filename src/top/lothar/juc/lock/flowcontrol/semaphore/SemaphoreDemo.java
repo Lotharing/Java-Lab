d29451df6e2e9ac17a594b7b3691e764b2750f95
lutong <809676295@qq.com>
2020-09-12 16:06:57 +0800

    控制并发流程- Semaphore(信号量/许可证) - 普通用法：信号量5个 线程池中50个 业务执行前调用acquire()拿信号量 但同时只有5个线程会执行因为最多5个信号量 / 最终需要释放

---
+++ src/top/lothar/juc/lock/flowcontrol/semaphore/SemaphoreDemo.java
@@ -0,0 +1,42 @@
+package top.lothar.juc.lock.flowcontrol.semaphore;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * 描述：     演示Semaphore 信号量(许可证)用法
+ */
+public class SemaphoreDemo {
+    //permits 信号量数量  fair 公平非公平
+    static Semaphore semaphore = new Semaphore(5, true);
+
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newFixedThreadPool(50);
+        for (int i = 0; i < 100; i++) {
+            service.submit(new Task());
+        }
+        service.shutdown();
+    }
+
+    static class Task implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                semaphore.acquire();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + "拿到了许可证");
+            try {
+                //模拟执行任务
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + "释放了许可证");
+            semaphore.release();
+        }
+    }
+}
